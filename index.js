fabef71d3e80ac1754290637df4f4fc7bc042011
reffypradana10 <reffypradana10@gmail.com>
2023-09-16 02:34:11 +0700

    Menjalankan file pertama"

---
+++ index.js
@@ -0,0 +1,9 @@
+// const nama = 'refi'
+// const cetakNama = (nama) => `Hi, nama saya ${nama}`
+// console.log(cetakNama('refi'))
+
+const cetakNama = require('./coba')
+
+
+
+console.log(cetakNama('refi'));
